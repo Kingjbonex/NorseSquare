13dbaf214c30c0dbe1d378acad8081908d4cee99
Brit Indrelie <brit@Thorlack.(none)>
2012-10-28 17:49:27 -0500

    added new files

---
+++ website/maps/mapStyles.js
@@ -0,0 +1,15 @@
+
+
+var greenStyle = [
+  {
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },{
+    stylers: [
+      { hue: "#004cff" },
+      { saturation: -42 }
+    ]
+  }
+];
---
+++ website/maps/treeview.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(document).ready(function(){
+	$("#black").treeview({
+		animated: "fast",
+		collapsed: true,
+		unique: true,
+		persist: "cookie",
+		toggle: function() {
+			window.console && console.log("%o was toggled", this);
+		}
+	});
+});
