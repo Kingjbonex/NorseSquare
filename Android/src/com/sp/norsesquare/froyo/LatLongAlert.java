d4e8c18aad91c15e37ade7cab15af0ae238aacf1
Tim Van Cleave <vancti01@luther.edu>
2013-02-23 15:20:07 -0600

    COmmit prior to actual upload, sorry

---
+++ Android/src/com/sp/norsesquare/froyo/LatLongAlert.java
@@ -0,0 +1,24 @@
+package com.sp.norsesquare.froyo;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+public class LatLongAlert extends AlertDialog {
+
+	public LatLongAlert(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LatLongAlert(Context context, int theme) {
+		super(context, theme);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LatLongAlert(Context context, boolean cancelable,
+			OnCancelListener cancelListener) {
+		super(context, cancelable, cancelListener);
+		// TODO Auto-generated constructor stub
+	}
+
+}
