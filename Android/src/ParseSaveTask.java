22340f564cf5e9384606b15773b77ccdbb19c7c7
Tim Van Cleave <vancti01@luther.edu>
2013-02-23 15:20:30 -0600

    Commit ParseSaveTask.java

---
+++ Android/src/ParseSaveTask.java
@@ -0,0 +1,20 @@
+import android.os.AsyncTask;
+
+
+public class ParseSaveTask extends AsyncTask<String, Integer, String> {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected String doInBackground(String... arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
