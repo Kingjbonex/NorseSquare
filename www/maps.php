7d1ac6caa35adfe9e9c35b94444d0a6791bfa503
James Penning <pennja01@luther.edu>
2013-04-04 19:40:22 -0500

    Changed a few formatting issues. Is test to see how changes work on my Windows 8 Git.

--- www/maps.php
+++ www/maps.php
@@ -41,65 +41,57 @@
 ?>
 
 <script type="text/javascript">
-  var email = "<?php   if(isset($_POST['token'])){Print($email);} ?>";
-  var fname = "<?php   if(isset($_POST['token'])){Print($fname);} ?>";
-  var lname = "<?php   if(isset($_POST['token'])){Print($lname);} ?>";
-  var gid = "<?php   if(isset($_POST['token'])){Print($gid);} ?>";
+	var email = "<?php   if(isset($_POST['token'])){Print($email);} ?>";
+	var fname = "<?php   if(isset($_POST['token'])){Print($fname);} ?>";
+	var lname = "<?php   if(isset($_POST['token'])){Print($lname);} ?>";
+	var gid = "<?php   if(isset($_POST['token'])){Print($gid);} ?>";
 </script>
-
-
-
 
 <html> 
 <head> 
 	<link rel="stylesheet" type="text/css" href="stylesheet.css" />
-   <meta http-equiv="content-type" content="text/html; charset=UTF-8"/> 
-   <title>NorseSquare</title> 
-
-
-
-<script type="text/javascript" src="jquery.js"></script>
-<script type="text/javascript">
-(function() {
-    if (typeof window.janrain !== 'object') window.janrain = {};
-    if (typeof window.janrain.settings !== 'object') window.janrain.settings = {};
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8"/> 
+	<title>NorseSquare</title> 
+	<script type="text/javascript" src="jquery.js"></script>
+	<script type="text/javascript">
+    (function() {
+        if (typeof window.janrain !== 'object') window.janrain = {};
+        if (typeof window.janrain.settings !== 'object') window.janrain.settings = {};
+        
+        janrain.settings.tokenUrl = 'http://norsesquare.internal.luther.edu/maps.php';//switch to internal server
     
-    janrain.settings.tokenUrl = 'http://norsesquare.internal.luther.edu/maps.php';//switch to internal server
-
-    function isReady() { janrain.ready = true; };
-    if (document.addEventListener) {
-      document.addEventListener("DOMContentLoaded", isReady, false);
-    } else {
-      window.attachEvent('onload', isReady);
-    }
-
-    var e = document.createElement('script');
-    e.type = 'text/javascript';
-    e.id = 'janrainAuthWidget';
-
-    if (document.location.protocol === 'https:') {
-      e.src = 'https://rpxnow.com/js/lib/luther-bargain-books/engage.js';
-    } else {
-      e.src = 'http://widget-cdn.rpxnow.com/js/lib/luther-bargain-books/engage.js';
-    }
-
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(e, s);
-
-    //Calling function to create new user
-    if(email != "") {jQuery.post("./services/newUser.php", {fname:fname, lname:lname, email:email, gid:gid});}
-})();
-</script>
-
-
+        function isReady() { janrain.ready = true; };
+        if (document.addEventListener) {
+          document.addEventListener("DOMContentLoaded", isReady, false);
+        } else {
+          window.attachEvent('onload', isReady);
+        }
+    
+        var e = document.createElement('script');
+        e.type = 'text/javascript';
+        e.id = 'janrainAuthWidget';
+    
+        if (document.location.protocol === 'https:') {
+          e.src = 'https://rpxnow.com/js/lib/luther-bargain-books/engage.js';
+        } else {
+          e.src = 'http://widget-cdn.rpxnow.com/js/lib/luther-bargain-books/engage.js';
+        }
+    
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(e, s);
+    
+        //Calling function to create new user
+        if(email != "") {jQuery.post("./services/newUser.php", {fname:fname, lname:lname, email:email, gid:gid});}
+    })();
+    </script>
 
 </head> 
 <body onLoad="starter();">     
 	<div id="mainDiv">
-  <a id="janrainLink" class="janrainEngage" href="#"></a>
+	<a id="janrainLink" class="janrainEngage" href="#"></a>
 	<div id="map" style="width: 100%; height: 600px; float:right;"></div> 
   
-  </div>   
+	</div>   
 </body> 
 
    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
@@ -107,9 +99,6 @@
    <script type="text/javascript" src="polygons.js"> </script>
    <script type="text/javascript" src="mapStyles.js"> </script>
    <script type="text/javascript" src="maps.js"></script>
-
-
-
 </html>
 
 <?php 
