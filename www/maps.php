3e6984481b379f89588d97fbc81cb55e83ad9360
James Penning <pennja01@luther.edu>
2013-04-04 20:51:56 -0500

    removed button images

--- www/maps.php
+++ www/maps.php
@@ -100,9 +100,9 @@
         <div id="navBar">
         	<div id="tabs">
             	<ul>
-                	<li><a href="#friends"><img src="FriendsButton.png" alt="friend button" /></a></li>
-                    <li><a href="#plans"><img src="PlansButton.png" alt="plans button" /></a></li>
-                    <li><a href="#settings"><img src="SettingsButton.png" alt="settings button" /></a></li>
+                	<li><a href="#friends"></a></li>
+                    <li><a href="#plans"></a></li>
+                    <li><a href="#settings"></a></li>
                 </ul>
                 <div id="friends">
                     <p>Where our friends section will be placed.</p>
