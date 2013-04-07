251bb218d0bcd5a8a9d6f36fed326d0fbbcc20b2
NorseSquare <norsesquare@norsesquare.(none)>
2013-04-07 15:28:54 -0500

    fixed resizing of google map through toggle button click.

--- www/ui.js
+++ www/ui.js
@@ -2,9 +2,16 @@ $('#tabs').tabs();
 
 $('button').click(
 
-function() {
-	$('#map').toggle()
-}, function() {
-	$('#tabs').toggle()
-	$('#map').toggleClass('extend-map');
-}, google.maps.event.trigger(map,'resize'));
+	function() {
+		$('#map').toggle();
+		google.maps.event.trigger(map, 'resize');
+		map.setZoom( map.getZoom() );
+	}, 
+
+	function() {
+		$('#tabs').toggle();
+		$('#map').toggleClass('extend-map');
+		google.maps.event.trigger(map, 'resize');
+		map.setZoom( map.getZoom() );
+	}
+);
