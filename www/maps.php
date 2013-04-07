1342fdd2a1950967b8e12c25da5ac5964e7a3e28
James Penning <pennja01@luther.edu>
2013-04-07 16:36:22 -0500

    Deleted extra paren.

--- www/maps.php
+++ www/maps.php
@@ -109,7 +109,7 @@
 					data: {UID:gid},
 					async: false, 
 					success: function(data){
-						if(!data){friendImage = 'http://maps.google.com/mapfiles/marker.png');}
+						if(!data){friendImage = 'http://maps.google.com/mapfiles/marker.png'}
 						else {friendImage = data};
 					}
 				}
