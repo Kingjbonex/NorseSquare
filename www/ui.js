3cca101369e3604c015cdd4368cbbd03359925c5
James Penning <pennja01@luther.edu>
2013-04-18 22:02:52 -0500

    Merged functions for cleaner code.

--- www/ui.js
+++ www/ui.js
@@ -11,19 +11,22 @@ $(function() {
 	$('#toggleButText')
 	.button()
 	.click(function() {
-		var buttonText = $(".ui-button-text").html();
-		if (buttonText == "&lt;<br>&lt;") $(".ui-button-text").html("&gt;<br>&gt;");
-		else $(".ui-button-text").html("&lt;<br>&lt;");
-	})
-	.click(function() {
+		var buttonText = $("#toggleButText").html();
+		if (buttonText == "<span class='ui-button-text'>&gt;<br>&gt;</span>") $("#toggleButText").html("<span class='ui-button-text'>&lt;<br>&lt;</span>");
+		else $("#toggleButText").html("<span class='ui-button-text'>&gt;<br>&gt;</span>");
+	});
+});
+
+
+$('#toggleButText').click(
+	function() {
 		$('#map').toggle();
 		google.maps.event.trigger(map, 'resize');
 		map.setZoom( map.getZoom() );
-	})
-	.click(function() {
+	},
+	function() {
 		$('#tabs').toggle();
 		$('#map').toggleClass('extend-map');
 		google.maps.event.trigger(map, 'resize');
 		map.setZoom( map.getZoom() );
 	});
-});
