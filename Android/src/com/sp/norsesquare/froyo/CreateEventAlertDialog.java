c81d5b13d3cce8a6a13d2c4f60851735295f02b8
Tim Van Cleave <vancti01@luther.edu>
2013-04-29 23:25:44 -0500

    fixed some merge issues, added back a deleted method.

--- Android/src/com/sp/norsesquare/froyo/CreateEventAlertDialog.java
+++ Android/src/com/sp/norsesquare/froyo/CreateEventAlertDialog.java
@@ -3,6 +3,7 @@ package com.sp.norsesquare.froyo;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 
@@ -22,8 +23,20 @@ public class CreateEventAlertDialog extends AlertDialog
 		AlertDialog.Builder builder = new AlertDialog.Builder(getOwnerActivity());
 		LayoutInflater inflater = getOwnerActivity().getLayoutInflater();
 		
-		builder.setView(inflater.inflate(R.layout.create_event_dialog,null));
-		builder.setTitle(R.string.title_create_event);
+//		builder.setView(inflater.inflate(R.layout.create_event_dialog,null));
+//		builder.setTitle(R.string.title_create_event);
+		
+		builder.setMessage("Only A Sith Deals in Absolutes")
+        .setPositiveButton("You Are A Good Person", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // FIRE ZE MISSILES!
+            }
+        })
+        .setNegativeButton("You Are A Coward", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
 		
 		
 		return builder.create();
