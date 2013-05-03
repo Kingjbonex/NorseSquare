9fc7a0d37543ce0965b84a7e73ab8be4220adbea
indrbr01 <indrbr01@luther.edu>
2013-05-02 21:56:39 -0500

    working on friends

--- www/maps.php
+++ www/maps.php
@@ -157,7 +157,7 @@
 						pending = $(this).find("pending").text(),
 						plusUrl = "http://plus.google.com/" + usergid;
 						if (gid != usergid) {
-							if (pending == 1){
+							if (pending == 2){
 								$('#friends-list-item-container').append('<div class="list-item"><div class="profile-image"><a href="' + plusUrl + '" target="_blank"><img src="' + friendImage + '"></a></div><div class="list-item-text"><span class="name">'+ fname + " " + lname + " "+'</span></div></div><div class="accept-request"><button id="accept-request-button">"Accept Request"</button></div>'); 
 								$(".accept-request").button({
 									text: true
