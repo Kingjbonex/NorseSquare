9342b6f4efb74331743e8c6566eaeaeb2e43e426
NorseSquare <norsesquare@norsesquare.(none)>
2013-05-02 19:41:35 -0500

    Services changes
    	modified:   www/services/getFriends.php
    	modified:   www/services/login.php
    	modified:   www/services/request.php

--- www/services/getFriends.php
+++ www/services/getFriends.php
@@ -7,9 +7,7 @@ if (!$connection)
 	echo "Could not connect: " . mysql_error();
 }
 mysql_select_db(DB_NAME, $connection);
-$start = (intval($page)-1)*30;
-$end =  intval($page)*30;
-$Query = 'SELECT users.uid,fname,lname FROM users INNER JOIN frequests ON users.uid=frequests.uid WHERE fuid='.$uid.' AND pending=0';
+$Query = 'SELECT users.uid,fname,lname,latitude,longitude,time,photourl FROM users INNER JOIN frequests ON users.uid=frequests.uid WHERE fuid='.$uid.' AND pending=0';
 //echo $Query;
 $result = mysql_query($Query,$connection);
 $gotarray = mysql_fetch_array($result);
