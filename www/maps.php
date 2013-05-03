8fd50b9c79554753008fc0ac9a0d72479eaece27
indrbr01 <indrbr01@luther.edu>
2013-05-02 22:34:49 -0500

    working on friends

--- www/maps.php
+++ www/maps.php
@@ -224,7 +224,7 @@ function acceptRequest(myUid,friendUid){
 						friendImage = $(this).find("photourl").text(),
 						plusUrl = "http://plus.google.com/" + usergid;
 						if (gid != usergid) {
-							$('#users-list-item-container').append('<div class="list-item"><div class="profile-image"><a href="' + plusUrl + '" target="_blank"><img src="' + friendImage + '"></a></div><div class="list-item-text"><span class="name">'+ fname + " " + lname + '</span></div><div class="right-button-icon"><button class="icon-button-"' + i + '"/></button></div></div>'); 
+							$('#users-list-item-container').append('<div class="list-item"><div class="profile-image"><a href="' + plusUrl + '" target="_blank"><img src="' + friendImage + '"></a></div><div class="list-item-text"><span class="name">'+ fname + " " + lname + '</span></div><div class="right-button-icon"><button class="icon-button-' + i + '"/></button></div></div>'); 
 							$(".icon-button-"+i).button({ icons: { primary: "ui-icon-circle-plus" }, text: false }).click(function(){sendRequest(userId,uid);});
 						}
 						i++;
