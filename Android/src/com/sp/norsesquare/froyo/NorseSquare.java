74c18807d6e484900904f73f6b705a8f1637f157
kuntza01 <kuntza01@luther.edu>
2013-05-02 20:56:34 -0500

    Login Task commit

--- Android/src/com/sp/norsesquare/froyo/NorseSquare.java
+++ Android/src/com/sp/norsesquare/froyo/NorseSquare.java
@@ -133,9 +133,14 @@ ConnectionCallbacks, OnConnectionFailedListener, DialogInterface.OnClickListener
         	
         }
         
+       // SECTION TO LOG IN 
+        //LOGGING INTO GOOGLE
        //Get authToken for luther.edu account 
        AsyncTask<String, Void, String> LoginTask = new LoginAsyncTask(lutherAccount,this).execute();
        Log.i("GOOGLEAUTH","AuthToken is: " + googleAuthToken);   
+       
+      
+       
        
        //new UserInfoAsyncTask(googleAuthToken).execute();
        //Initialize PlusClient
@@ -160,9 +165,17 @@ ConnectionCallbacks, OnConnectionFailedListener, DialogInterface.OnClickListener
     
         
         Log.i(TAG, "OnCreate");
+        
+        
+        
     }
 
-    @Override
+    private void LoginToDatabase() {
+		// TODO Auto-generated method stub
+    	AsyncTask<String, Void, Integer> LoginDatabase = new LoginDatabaseTask(me.getFirstName(), me.getLastName(), me.getEmail(), googleAuthToken).execute();
+	}
+
+	@Override
     protected void onResume() 
     {
         super.onResume();
@@ -183,6 +196,8 @@ ConnectionCallbacks, OnConnectionFailedListener, DialogInterface.OnClickListener
 		  
 	  	super.onStart();
 
+	  	
+	  	
 	  	// obtain location manager at restart of activity
 	  	locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 	  	
@@ -788,7 +803,7 @@ ConnectionCallbacks, OnConnectionFailedListener, DialogInterface.OnClickListener
 	        	
 	        	//Create new marker with user's information. Add to storedMarkerList.
 	        	LatLng locP = new LatLng(latitude,longitude);
-	        	MapMarker newmark = new MapMarker(locP, fname+" "+lname, "checked in on "+ gtime);
+	        	MapMarker newmark = new MapMarker(locP, fname+" "+lname, "checked in "+ gtime+" ago");
 	        	Log.i("FINDALL",fname);
 	        	//Toast.makeText(this, "Adding found to Marker List", Toast.LENGTH_SHORT).show();
 	        	storedMarkerList.add(newmark);
@@ -882,6 +897,7 @@ public class LoginAsyncTask extends AsyncTask<String, Void, String>
 		
         Log.i("GOOGLEAUTH", "Returning Received Google Token");
         googleAuthToken = result;
+        LoginToDatabase();
        
     }
 	
