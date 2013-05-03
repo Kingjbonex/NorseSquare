9893debeedf66691b876f51c20543dc13aeda143
Charles Banta <bantch01@luther.edu>
2013-05-02 22:44:34 -0500

    Took out old rightclick conflicting.

--- www/maps.js
+++ www/maps.js
@@ -340,12 +340,7 @@ function initialise(){
 	});
 }
 
-google.maps.event.addListener(map, "rightclick", function(event) {
-    var lat = event.latLng.lat();
-    var lng = event.latLng.lng();
-    // populate yor box/field with lat, lng
-    alert("Lat=" + lat + "; Lng=" + lng);
-});
+
 
 
 
