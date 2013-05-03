ce20d2e06854d27b143d23c8187213ab1d9f731b
Joel Anderson <andejo18@luther.edu>
2013-05-02 21:55:08 -0500

    More Help Menu additions, changed some colors of buttons - Joel

---
+++ Android/src/com/sp/norsesquare/froyo/HelpDialogBoxThree.java
@@ -0,0 +1,53 @@
+package com.sp.norsesquare.froyo;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+public class HelpDialogBoxThree extends DialogFragment {
+//	ListView listView;
+	
+	View helpView;
+	
+	TextView helpText;
+	
+	String[] listCont;
+	
+	Context context;
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) 
+	{
+		//Create Activity
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		LayoutInflater inflater = getActivity().getLayoutInflater();
+		
+		context = getActivity();
+				
+		helpView = (View) inflater.inflate(R.layout.help_dialog_three, null);
+		
+		helpText = (TextView) helpView.findViewById(R.id.helpTextViewThree);
+		
+		helpText.setMovementMethod(new ScrollingMovementMethod());
+		
+//		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_view_row, R.id.listText, listCont);               
+//		
+//		listView.setAdapter(adapter);
+//		
+//		listView.setOnItemClic
+		
+		builder.setView(helpView);
+		builder.setTitle("HELP: MARKERS");
+		
+//		eventList = (ListView) v.findViewById()
+	
+		return builder.create();
+//		return super.onCreateDialog(savedInstanceState);
+	}
+}
